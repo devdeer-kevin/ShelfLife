895116f0d4cf1eeaa6d13e3bd7bde90f6e5ae075
devdeer-kevin <kevin.heyland@devdeer.com>
2024-03-23 15:53:32 +0100

    Add doc

--- BaseTypes/Book.cs
+++ BaseTypes/Book.cs
@@ -3,8 +3,19 @@ namespace ShelfLife.BaseTypes
 
     public class Book
     {
+        //summary
+        // The title of the book.
+        //summary
         public string? Title { get; set; }
+        
+        //summary
+        // The price of the book.
+        //summary
         public int? Price { get; set; }
+        
+        //summary
+        // The ISBN of the book.
+        //summary
         public string? ISBN { get; set; }
     }
 }
